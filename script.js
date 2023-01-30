d054b97248b8f3c5f0e36f65986d39c46133514e
Hurrrrrr <jonahpalmer@gmail.com>
2023-01-30 00:18:21 -0500

    finish a few functions, begin pop

--- script.js
+++ script.js
@@ -9,8 +9,7 @@ const LinkedListFactory = () => {
     const append = (value) => {
 
         // create a new node with the given value
-        let newNode = NodeFactory();
-        newNode.value = value;
+        let newNode = NodeFactory(value);
 
         // if the LL has no head yet, designate the new node as head
         if (this.head === null) {
@@ -30,31 +29,76 @@ const LinkedListFactory = () => {
     };
 
     // add a node with a value to the start of the LL
-    // const prepend = (value) => {
+    const prepend = (value) => {
 
-    //     // create a new node with the given value
+        // create a new node with the given value
+        let newNode = NodeFactory(value);
 
+        // if the LL already has a head, point the new node to it
+        if (this.head !== null) {
+            newNode.next = this.head;
+        }
 
-    //     // if the LL has no head yet, designate the new node as head
+        this.head = newNode;
 
-
-    //     // point the new node to the previous head
-    // }
+        return newNode;
+    }
 
 
     // count the number of nodes in the LL
+    const size = () => {
 
+        let count = 0;
+
+        let trav = this.head;
+        while (trav) {
+            count++;
+            trav = trav.next;
+        }
+
+        return count;
+    }
 
     // return the first node of the LL
+    const headF = () => {
+        return this.head;
+    }
 
 
     // return the last node of the LL
-
+    const tailF = () => {
+        return this.tail;
+    }
 
     // return the node at index (n)
+    const at = (index) => {
+
+        let trav = this.head;
+
+        for (let i = 0; i < index; i++) {
+            trav = trav.next;
+        }
+
+        return trav;
+    }
 
 
     // remove the last node from the LL
+    const pop = () => {
+
+        this.tail = null;
+
+        // traverse the LL to find the new tail
+        let trav = this.head;
+        while (trav) {
+
+            trav = trav.next;
+            // if we've found the new tail, designate it
+            if (trav.next === null) {
+                this.tail = trav;
+            }
+        }
+    }
 
 
     // return true if the passed value is found in the LL
@@ -68,17 +112,20 @@ const LinkedListFactory = () => {
 
     // remove node at index (n)
 
-    return { append };
+    return { append, prepend, size, headF, tailF, at, pop };
+
 }
 
-const NodeFactory = () => {
+const NodeFactory = (value) => {
     this.next = null;
-    this.value = null;
+    this.value = value;
 
     return { next, value };
 }
 
 const list = LinkedListFactory();
 
-list.append(5);
-list.append(3);
+list.append("a");
+list.append("b");
+list.append("c");
+list.append("d");
